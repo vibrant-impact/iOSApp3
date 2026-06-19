e21f055078eaa5792129db9db0a0b4a8df57824c
Stephanie Otteson <vibrantimpact@myyahoo.com>
2026-06-18 18:13:19 -0600

    Initial Commit

---
+++ iOSApp3/iOSApp3App.swift
@@ -0,0 +1,17 @@
+//
+//  iOSApp3App.swift
+//  iOSApp3
+//
+//  Created by stephanie otteson on 2026-06-18.
+//
+
+import SwiftUI
+
+@main
+struct iOSApp3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
